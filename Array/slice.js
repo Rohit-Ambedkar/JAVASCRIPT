21a7c627b78d50f1a2f59d6b3cdd34bcf13d45e6
Rohit kumar Ambedkar <ambedkarrohit608@gmail.com>
2026-05-25 01:15:33 +0530

    Initial commit

---
+++ Array/slice.js
@@ -0,0 +1,5 @@
+// Slice in Js ==> copies a portion of an array
+
+let colors =["red","yellow","green","blue"];
+console.log(colors.length); // [ 'red', 'yellow', 'green', 'blue' 
+console.log(colors.slice(-1)); // [ 'red', 'yellow', 'green', 'blue' ]
