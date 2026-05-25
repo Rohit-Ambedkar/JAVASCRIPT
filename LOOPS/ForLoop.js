5893afd5ffbff427b3eaf00d560981ae681fc61d
Rohit kumar Ambedkar <ambedkarrohit608@gmail.com>
2026-05-26 00:07:33 +0530

    add loops file

---
+++ LOOPS/ForLoop.js
@@ -0,0 +1,37 @@
+
+// for(let i=0;i<=100;i++){
+    // console.log(i);
+    
+// }
+
+// Q1. Print all odd numbers (1 to 15)
+
+// Ans 1
+// for(let i=1;i<=15;i+=2){
+    // console.log(i);
+    
+// }
+
+
+// Ans 2
+// for(let i=1;i<=15;i++){
+    // if(i%2!=0){ // condition for print all odd numbers.
+        // console.log(i);
+    // }
+// }
+
+
+// Q2. print all even number (2 to 10)
+
+// for(let i=2;i<=10;i+=2){
+    // console.log(i);
+    
+// }
+
+
+// Q3. print the multiplication table for 5
+
+// for(let i=5;i<=50;i+=5){
+    // console.log(i);
+    
+// }
